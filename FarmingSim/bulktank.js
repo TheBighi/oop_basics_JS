eca382755be217e54eb015bfbb671842e1ab3cd4
TheBighi <thebighix@gmail.com>
2025-09-29 09:15:00 +0300

    fixed

--- FarmingSim/bulktank.js
+++ FarmingSim/bulktank.js
@@ -1,27 +1,27 @@
-class BulkTank{
-    constructor(capacity=2000){
+class BulkTank {
+    constructor(capacity=2000) {
         this.capacity = capacity
         this.volume = 0
     }
-    GetCapacity(){
+    GetCapacity() {
         return this.capacity
     }
-    GetVolume(){
+    GetVolume() {
         return this.volume
     }
-    howMuchFreeSpace(){
+    howMuchFreeSpace() {
         return this.capacity - this.volume
     }
-    addToTank(amount){
-        if (this.volume + amount > this.capacity){
+    addToTank(amount) {
+        if (this.volume + amount > this.capacity) {
             console.log("Tank cannot fit anymore")
         }
         else{
             this.volume += amount;
         }
     }
-    getFromTank(amount){
-        if (this.volume - amount < 0){
+    getFromTank(amount) {
+        if (this.volume - amount < 0) {
             console.log("Cannot take this amount from tank because not enough")
         }
         else{
--- FarmingSim/farm.js
+++ FarmingSim/farm.js
@@ -1,9 +1,9 @@
 const BulkTank  = require('./bulktank');
 
-const tank1 = new BulkTank(100)
+const tank1 = new BulkTank()  // 2000
 
 console.log(tank1.GetCapacity())
-tank1.addToTank(10)
+tank1.addToTank(7)
 console.log(tank1.GetVolume())
 tank1.getFromTank(5)
 console.log(tank1.GetVolume())
