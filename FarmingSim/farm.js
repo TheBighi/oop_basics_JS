f17648624c1fd5e9d9967ac3735c845b4cbc6b50
TheBighi <thebighix@gmail.com>
2025-09-29 09:56:11 +0300

    Fixed

--- FarmingSim/farm.js
+++ FarmingSim/farm.js
@@ -1,14 +1,29 @@
-const BulkTank  = require('./bulktank');
-const Cow  = require('./cow');
+const BulkTank  = require('./bulktank.js');
+const Cow  = require('./cow.js');
 
 const tank1 = new BulkTank()  // 2000
-const cow1 = new Cow("Awesome cow")
 
 console.log(tank1.GetCapacity())
 tank1.addToTank(7)
 tank1.getFromTank(5)
 tank1.print()
+console.log("-------COW STARTS-------")
+const cow = new Cow("Awesome cow");
+cow.print();
 
-console.log(cow1.GetAmount())
-console.log(cow1.GetCapacity())
-cow1.print()
+/** @type {Alive} */ (cow).liveHour();
+cow.liveHour();
+cow.liveHour();
+cow.liveHour();
+cow.print();
+
+/** @type {Milkable} */ (cow).milk();
+console.log("");
+
+const cow1 = new Cow("Ammu");
+cow1.print();
+cow1.liveHour();
+cow1.liveHour();
+cow1.print();
+cow1.milk();
+cow1.print();
