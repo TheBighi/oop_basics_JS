4b039281e70206211183517e343a5dc075443195
TheBighi <thebighix@gmail.com>
2025-09-29 09:19:06 +0300

    made private

--- FarmingSim/bulktank.js
+++ FarmingSim/bulktank.js
@@ -1,31 +1,34 @@
 class BulkTank {
+    #capacity = 2000
+    #volume = 0
+    
     constructor(capacity=2000) {
-        this.capacity = capacity
-        this.volume = 0
+        this.#capacity = capacity
+        this.#volume = 0
     }
     GetCapacity() {
-        return this.capacity
+        return this.#capacity
     }
     GetVolume() {
-        return this.volume
+        return this.#volume
     }
     howMuchFreeSpace() {
-        return this.capacity - this.volume
+        return this.#capacity - this.#volume
     }
     addToTank(amount) {
-        if (this.volume + amount > this.capacity) {
+        if (this.#volume + amount > this.#capacity) {
             console.log("Tank cannot fit anymore")
         }
         else{
-            this.volume += amount;
+            this.#volume += amount;
         }
     }
     getFromTank(amount) {
-        if (this.volume - amount < 0) {
+        if (this.#volume - amount < 0) {
             console.log("Cannot take this amount from tank because not enough")
         }
         else{
-            this.volume -= amount
+            this.#volume -= amount
         }
     }
 }
