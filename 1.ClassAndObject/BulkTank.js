f93db6aed95a22c0cdb56ddca1c47381a53cf44e
TheBighi <thebighix@gmail.com>
2025-09-29 08:59:04 +0300

    Added BulkTank class to upcoming farm sim

---
+++ 1.ClassAndObject/BulkTank.js
@@ -0,0 +1,31 @@
+class BulkTank{
+    constructor(capacity=2000){
+        this.capacity = capacity
+        this.volume = 0
+    }
+    GetCapacity(){
+        return this.capacity
+    }
+    GetVolume(){
+        return this.volume
+    }
+    howMuchFreeSpace(){
+        return this.capacity - this.volume
+    }
+    addToTank(amount){
+        if (this.volume + amount > this.capacity){
+            console.log("Tank cannot fit anymore")
+        }
+        else{
+            this.volume + amount
+        }
+    }
+    getFromTank(amount){
+        if (this.volume - amount < 0){
+            console.log("Cannot take this amount from tank because not enough")
+        }
+        else{
+            this.volume - amount
+        }
+    }
+}
